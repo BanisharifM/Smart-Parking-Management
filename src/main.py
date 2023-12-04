97ff20ecffd14991a51690469de9601b2a425180
MahdiBD <msharif@iastate.edu>
2023-12-04 05:44:26 -0600

    result is finished

--- src/main.py
+++ src/main.py
@@ -55,14 +55,14 @@ def load_s3_file_structure(path: str = "src/all_image_files.json") -> dict:
         return json.load(f)
 
 
-# @st.cache_data()
-# def load_list_of_images_available(
-#     all_image_files: dict, image_files_dtype: str, bird_species: str
-# ) -> list:
-#     """Retrieves list of available images given the current selections"""
-#     species_dict = all_image_files.get(image_files_dtype)
-#     list_of_files = species_dict.get(bird_species)
-#     return list_of_files
+@st.cache_data()
+def load_list_of_images_available(
+    all_image_files: dict, image_files_dtype: str, bird_species: str
+) -> list:
+    """Retrieves list of available images given the current selections"""
+    species_dict = all_image_files.get(image_files_dtype)
+    list_of_files = species_dict.get(bird_species)
+    return list_of_files
 
 
 @st.cache_data()
@@ -119,13 +119,25 @@ def cal_classes_counts(total_detections, detect_params, class_list):
     return class_counts
 
 
+# def cal_classes_percentage(total_detections, class_counts):
+#     class_percentages = {
+#         class_name: count / total_detections * 100
+#         for class_name, count in class_counts.items()
+#     }
+#     for class_name in class_counts.items():
+#         print(f"Percentage of {class_name}: {class_percentages[class_name]:.2f}%")
+
+
 def cal_classes_percentage(total_detections, class_counts):
     class_percentages = {
         class_name: count / total_detections * 100
         for class_name, count in class_counts.items()
     }
-    for class_name in class_counts.items():
-        print(f"Percentage of {class_name}: {class_percentages[class_name]:.2f}%")
+
+    for class_name, percentage in class_percentages.items():
+        print(f"Percentage of {class_name}: {percentage:.2f}%")
+
+    return class_percentages
 
 
 def save_uploaded_image(file, uploaded_path):  # if user uploaded file
@@ -136,20 +148,7 @@ def save_uploaded_image(file, uploaded_path):  # if user uploaded file
     st.write("File saved to:", file_path)
 
 
-# def save_image(image, output_path):
-#     output_directory = os.path.dirname(output_path)
-#     os.makedirs(output_directory, exist_ok=True)
-
-#     cv2.imwrite(output_path, image)
-
-
-#     if os.path.exists(output_path):
-#         print(f"Image saved successfully to: {output_path}")
-#     else:
-#         print("Failed to save the image.")
-
-
-def save_image(image,image_name, output_path):
+def save_image(image, image_name, output_path):
     # Ensure the directory exists, create it if it doesn't
     output_directory = os.path.dirname(output_path)
     os.makedirs(output_directory, exist_ok=True)
@@ -158,7 +157,7 @@ def save_image(image,image_name, output_path):
     valid_extensions = [".jpg", ".jpeg", ".png"]  # Add more extensions if needed
     ext = os.path.splitext(output_path)[1].lower()
     if ext not in valid_extensions:
-        output_path = os.path.splitext(output_path)[0] + image_name + ".jpg"
+        output_path = os.path.splitext(output_path)[0] + image_name
 
     # Save the image
     cv2.imwrite(output_path, image)
@@ -170,11 +169,6 @@ def save_image(image,image_name, output_path):
         print("Failed to save the image.")
 
 
-# Example usage:
-# Assuming you have an image 'frame' and a desired output path 'output_path'
-# save_image(frame, "/path/to/new_directory/saved_frame.jpg")
-
-
 if __name__ == "__main__":
     uploaded_path = "uploaded_images/"
     predicted_path = "predicted_images/"
@@ -182,12 +176,12 @@ if __name__ == "__main__":
     model = load_model()
     class_list = load_index_to_label_dict()
     all_image_files = load_s3_file_structure()
-    #     types_of_birds = sorted(list(all_image_files["test"].keys()))
-    #     types_of_birds = [bird.title() for bird in types_of_birds]
+    types_of_birds = sorted(list(all_image_files["test"].keys()))
+    types_of_birds = [bird.title() for bird in types_of_birds]
 
     detection_colors = [(252, 191, 73), (207, 233, 8)]
 
-    st.title("Welcome To Project Eagle Vision!")
+    st.title("Welcome To Smart Parking Vision!")
     instructions = """
         SPM(Smart Parking Management) aims to transform this using machine learning, specifically the YOLO v8 algorithm, to analyze parking images, count cars, and spot available spaces.
         """
@@ -203,9 +197,9 @@ if __name__ == "__main__":
     data_split_names = list(dtype_file_structure_mapping.keys())
 
     if file:
-        save_uploaded_image(file, uploaded_path)
+        img = Image.open(file)
 
-        # img = Image.open(file)
+        save_uploaded_image(file, uploaded_path)
 
         image_path = uploaded_path + file.name
         frame = cv2.imread(image_path)
@@ -217,7 +211,7 @@ if __name__ == "__main__":
         predicted_image = image_annotation(
             prediction, frame, class_list, detection_colors
         )
-        save_image(predicted_image, file.name,predicted_path)
+        save_image(predicted_image, file.name, predicted_path)
 
         total_detections = len(prediction[0]) if len(prediction[0]) != 0 else 1
 
@@ -227,56 +221,58 @@ if __name__ == "__main__":
         class_percentage = cal_classes_percentage(total_detections, class_counts)
         print("class_percentage", class_percentage)
 
-    # top_prediction = prediction[0][0]
-    # available_images = all_image_files.get("train").get(top_prediction.upper())
-    # examples_of_species = np.random.choice(available_images, size=3)
-    # files_to_get_from_s3 = []
+        # top_prediction = prediction[0][0]
+        # available_images = all_image_files.get("train").get(top_prediction.upper())
+        # examples_of_species = np.random.choice(available_images, size=3)
+        # files_to_get_from_s3 = []
 
-    # for im_name in examples_of_species:
-    #     path = os.path.join("train", top_prediction.upper(), im_name)
-    #     files_to_get_from_s3.append(path)
-    # images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
+        # for im_name in examples_of_species:
+        #     path = os.path.join("train", top_prediction.upper(), im_name)
+        #     files_to_get_from_s3.append(path)
+        # images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
 
-#     else:
-#         dataset_type = st.sidebar.selectbox("Data Portion Type", data_split_names)
-#         image_files_subset = dtype_file_structure_mapping[dataset_type]
+        #     st.title("Here is the image you've selected")
 
-#         selected_species = st.sidebar.selectbox("Bird Type", types_of_birds)
-#         available_images = load_list_of_images_available(
-#             all_image_files, image_files_subset, selected_species.upper()
-#         )
-#         image_name = st.sidebar.selectbox("Image Name", available_images)
-#         if image_files_subset == "consolidated":
-#             s3_key_prefix = "consolidated/consolidated"
-#         else:
-#             s3_key_prefix = image_files_subset
-#         key_path = os.path.join(s3_key_prefix, selected_species.upper(), image_name)
-#         files_to_get_from_s3 = [key_path]
-#         examples_of_species = np.random.choice(available_images, size=3)
+        file_path = os.path.join(predicted_path, file.name)
+        img = Image.open(file_path)
+        resized_image = img.resize((336, 336))
+        st.title("Here the image detected.")
+        st.image(resized_image)
+        df = pd.DataFrame(
+            data=np.zeros((5, 2)),
+            columns=["Species", "Confidence Level"],
+            index=np.linspace(1, 5, 5, dtype=int),
+        )
 
-#         for im in examples_of_species:
-#             path = os.path.join(s3_key_prefix, selected_species.upper(), im)
-#             files_to_get_from_s3.append(path)
-#         images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
-#         img = images_from_s3.pop(0)
-#         prediction = predict(img, index_to_class_label_dict, model, 5)
+        # for idx, p in enumerate(prediction):
+        #     link = "https://en.wikipedia.org/wiki/" + p[0].lower().replace(" ", "_")
+        #     df.iloc[idx, 0] = f'<a href="{link}" target="_blank">{p[0].title()}</a>'
+        #     df.iloc[idx, 1] = p[1]
+        st.write(df.to_html(escape=False), unsafe_allow_html=True)
+        st.title(f"Here are three other images of the {prediction[0][0]}")
 
-#     st.title("Here is the image you've selected")
-#     resized_image = img.resize((336, 336))
-#     st.image(resized_image)
-#     st.title("Here are the five most likely bird species")
-#     df = pd.DataFrame(
-#         data=np.zeros((5, 2)),
-#         columns=["Species", "Confidence Level"],
-#         index=np.linspace(1, 5, 5, dtype=int),
-#     )
+    else:
+        dataset_type = st.sidebar.selectbox("Data Portion Type", data_split_names)
+        image_files_subset = dtype_file_structure_mapping[dataset_type]
 
-#     for idx, p in enumerate(prediction):
-#         link = "https://en.wikipedia.org/wiki/" + p[0].lower().replace(" ", "_")
-#         df.iloc[idx, 0] = f'<a href="{link}" target="_blank">{p[0].title()}</a>'
-#         df.iloc[idx, 1] = p[1]
-#     st.write(df.to_html(escape=False), unsafe_allow_html=True)
-#     st.title(f"Here are three other images of the {prediction[0][0]}")
+        selected_species = st.sidebar.selectbox("Bird Type", types_of_birds)
+        available_images = load_list_of_images_available(
+            all_image_files, image_files_subset, selected_species.upper()
+        )
+        image_name = st.sidebar.selectbox("Image Name", available_images)
+        if image_files_subset == "consolidated":
+            s3_key_prefix = "consolidated/consolidated"
+        else:
+            s3_key_prefix = image_files_subset
+        key_path = os.path.join(s3_key_prefix, selected_species.upper(), image_name)
+        files_to_get_from_s3 = [key_path]
+        examples_of_species = np.random.choice(available_images, size=3)
 
+        for im in examples_of_species:
+            path = os.path.join(s3_key_prefix, selected_species.upper(), im)
+            files_to_get_from_s3.append(path)
+        images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
+        img = images_from_s3.pop(0)
+        prediction = predict(img, class_list, model, 5)
 #     st.image(images_from_s3)
 # st.title('How it works:')
